6cbf6fa7ca0c043e4d53bf3da8d963c3f31623bd
Maksaad002 <mak.saad02@gmail.com>
2023-04-05 16:51:20 -0700

    tasks

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * set_string - sets the value of a pointer to a char
- * @s: pointer to pointer
- * @to: pointer char
+ *set_string - a function that sets the value of a pointer to a char.
+ *@s: string to be sent
+ *@to: sent to this string
  */
 
 void set_string(char **s, char *to)
 {
-	*s = to;
+*s = to;
 }
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,25 +1,22 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * print_diagsums - Entry point
- * @a: input
- * @size: input
- * Return: Always 0 (Success)
+ *print_diagsums - prints the sum of dia
+ *@a: pointer that points
+ *@size: basically the size
+ *Return: return nothing at the moment
  */
+
 void print_diagsums(int *a, int size)
 {
-	int sum1, sum2, y;
-
-	sum1 = 0;
-	sum2 = 0;
-
-	for (y = 0; y < size; y++)
-	{
-		sum1 = sum1 + a[y * size + y];
-	}
-	for (y = size - 1; y >= 0; y--)
-	{
-		sum2 += a[y * size + (size - y - 1)];
-	}
-	printf("%d, %d\n", sum1, sum2);
+int sum1 = 0;
+int sum2 = 0;
+int i;
+for (i = 0; i < size; i++)
+{
+sum1 += a[i * (size + 1)];
+sum2 += a[(i + 1) * (size - 1)];
+}
+printf("%d, %d\n", sum1, sum2);
 }
